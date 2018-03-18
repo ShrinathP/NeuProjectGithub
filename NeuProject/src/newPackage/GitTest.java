c2b9c56c82aba0c61094c3accf71bdb2b584f7e1
s.sadanand.potul <s.sadanand.potul@M5-L-DJ4LBH2.dir.svc.accenture.com>
2018-03-18 18:49:49 +0530

    Initial checking of Git

---
+++ NeuProject/src/newPackage/GitTest.java
@@ -0,0 +1,18 @@
+package newPackage;
+
+public class GitTest {
+	
+	
+	public static void writeThis(String Subject) {
+		
+		System.out.println("This is the thing to write:"+ " "+Subject);
+	}
+
+	
+	public static void main (String[] args)
+	{
+		writeThis("Chemistry");
+		writeThis("Maths");
+
+	}
+}
---
+++ NeuProject/src/newPackage/GitTest2.java
@@ -0,0 +1,9 @@
+package newPackage;
+
+public class GitTest2 extends GitTest {
+	
+	public static void main (String[] args)
+	{writeThis("Biology");
+	}
+
+}
